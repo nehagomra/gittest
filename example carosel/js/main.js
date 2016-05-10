6fd9052b8466ef18d0c6dcb7e43c5914570ea22b
Neha Singh <Neha.Singh@prodigi.local>
2016-05-10 11:05:01 +0530

    my gittest folder

---
+++ example carosel/js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){$(window).load(function() {
+  $('.flexslider').flexslider({
+    animation: "slide"
+  });
+});
+
+
+
+$(document).ready(function(){
+    $("p").click(function(){
+        $(this).hide();
+    });
+});
